719f542d0b068049193d9f088e36279b7e89287f
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-12-08 18:28:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
-$response = http_get("http://www.example.com/", array("timeout"=>1), $info);
-print_r($info);
-?> 
+<?
+$client_id="GCE5QLHTHPTXWBWRTEOPX0IDVF5XPQJXPHRFLOZZZW5DY51P";
+$client_secret="Q3KYQNLRIR05WLGQHK2TGBOXDXZIGQA140DQ4KTIVWM4QNSH";
+$r = new HttpRequest('GET https://api.foursquare.com/v2/venues/search', HttpRequest::METH_GET);
+//$r->setOptions(array('lastmodified' => filemtime('local.rss')));
+$r->addQueryData(array('client_id' =>"$client_id".'&client_secret'=>"$client_secret".'&near=Bergamo,IT&query=pizza'));
+/*try {
+    $r->send();
+    if ($r->getResponseCode() == 200) {
+        file_put_contents('local.rss', $r->getResponseBody());
+    }
+} catch (HttpException $ex) {
+    echo $ex;
+}*/
+
+?>
