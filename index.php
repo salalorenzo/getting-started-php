59f52a6920326b7dca5a63ad8a6a9e42fdb0a30b
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-12-10 17:38:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,11 @@
 </head>
 <body background class='background'>
 <?
-// Get cURL resource
-$client_id="GCE5QLHTHPTXWBWRTEOPX0IDVF5XPQJXPHRFLOZZZW5DY51P";
-$client_secret="Q3KYQNLRIR05WLGQHK2TGBOXDXZIGQA140DQ4KTIVWM4QNSH";
-$url="https://api.foursquare.com/v2/venues/search?client_id=$client_id&client_secret=$client_secret&near=Bergamo,IT&v=20161016&query=pizza";
+$url="https://api.foursquare.com/v2/venues/search?client_id=GCE5QLHTHPTXWBWRTEOPX0IDVF5XPQJXPHRFLOZZZW5DY51P&client_secret=Q3KYQNLRIR05WLGQHK2TGBOXDXZIGQA140DQ4KTIVWM4QNSH&near=Bergamo,IT&v=20161016&query=pizza";
 $quanti=100;
-// Initialize session and set URL.
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-// Set so curl_exec returns the result instead of outputting it.
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-// Get the response and close the channel.
 $response = curl_exec($ch);
 
 $response=json_decode($json);
