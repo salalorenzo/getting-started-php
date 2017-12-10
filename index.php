f8b2273e35713bcc38fdad49829173314607ba7b
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-12-10 18:01:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,11 @@
 <head>
 <title>Pizzerie di Bergamo</title>
 <link href="stile.css" rel="stylesheet" type="text/css">
-<script>
-function funzione()
-	{	
-		var q=document.getElementById("quanti").value;
-		return q;
-	}
-</script>
 </head>
 <body background class='background'>
 <?
-if(isset($_POST["quanti"]))
-	{
-		$quanti=$_POST["quanti"];
-	}
-else
-	{
-		$quanti=10;
-	}
 $url="https://api.foursquare.com/v2/venues/search?client_id=GCE5QLHTHPTXWBWRTEOPX0IDVF5XPQJXPHRFLOZZZW5DY51P&client_secret=Q3KYQNLRIR05WLGQHK2TGBOXDXZIGQA140DQ4KTIVWM4QNSH&near=Bergamo,IT&v=20161016&query=pizza";
-$quanti=100;
+	
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
@@ -34,7 +19,7 @@ echo "<th>Nome</th>";
 echo "<th>Latitudine</th>";
 echo "<th>Longitudine</th>";
 echo "</tr>";
-for($k=0;$k<$quanti;$k++)
+for($k=0;$k<count(venues);$k++)
 {
 		echo("<tr>");
 		echo("<td>").$risposta->response->venues[$k]->name.+"</td>";
@@ -44,11 +29,7 @@ for($k=0;$k<$quanti;$k++)
 }
 echo("</table>");
 curl_close($ch);
-	
-echo "<form id='forma' method='post' onsubmit='return funzione();'><br/>";
-echo "Numero elementi (1-50):<input type='text' value='$quanti' name='quanti'id='quanti'/>";
-echo "<input type='submit' value='Ricerca'/>";	
-echo "</form>"
+
 ?>
 
 </body>
