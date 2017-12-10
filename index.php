5f06dc6a2038a5a18f934cb760cf9a090211632d
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-12-10 16:56:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
 <?
+// Get cURL resource
 $client_id="GCE5QLHTHPTXWBWRTEOPX0IDVF5XPQJXPHRFLOZZZW5DY51P";
 $client_secret="Q3KYQNLRIR05WLGQHK2TGBOXDXZIGQA140DQ4KTIVWM4QNSH";
-$r = new HttpRequest('GET https://api.foursquare.com/v2/venues/search', HttpRequest::METH_GET);
-//$r->setOptions(array('lastmodified' => filemtime('local.rss')));
-$r->addQueryData(array('client_id' =>"$client_id".'&client_secret'=>"$client_secret".'&near=Bergamo,IT&query=pizza'));
-/*try {
-    $r->send();
-    if ($r->getResponseCode() == 200) {
-        file_put_contents('local.rss', $r->getResponseBody());
-    }
-} catch (HttpException $ex) {
-    echo $ex;
-}*/
-echo("fatto");
 
+$url="https://api.foursquare.com/v2/venues/search?client_id=$client_id&client_secret=$client_secret&near=Bergamo,IT";
+
+// Initialize session and set URL.
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Set so curl_exec returns the result instead of outputting it.
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+// Get the response and close the channel.
+$response = curl_exec($ch);
+curl_close($ch);
+
+$d=json_decode($json);
+echo("<table>");
+echo("<tr>");
+echo("<td>").$d->response->venues[0]->name.+"</td>";
+echo("<td>").$d->response->venues[1]->lat.+"</td>";
+echo("<td>").$d->response->venues[2]->lng.+"</td>";
+echo("</tr>");
+echo("</table>");
 ?>
