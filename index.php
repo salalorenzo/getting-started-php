d29d5e72f226e653888a9c2de4a10fb94651a2e9
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-12-10 17:56:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 <head>
 <title>Pizzerie di Bergamo</title>
 <link href="stile.css" rel="stylesheet" type="text/css">
+<script>
+function funzione()
+	{	
+		var q=document.getElementById("quanti").value;
+		return q;
+	}
+</script>
 </head>
 <body background class='background'>
 <?
+if(isset($_POST["quanti"]))
+	{
+		$quanti=$_POST["quanti"];
+	}
+else
+	{
+		$quanti=10;
+	}
 $url="https://api.foursquare.com/v2/venues/search?client_id=GCE5QLHTHPTXWBWRTEOPX0IDVF5XPQJXPHRFLOZZZW5DY51P&client_secret=Q3KYQNLRIR05WLGQHK2TGBOXDXZIGQA140DQ4KTIVWM4QNSH&near=Bergamo,IT&v=20161016&query=pizza";
 $quanti=100;
 $ch = curl_init();
@@ -29,6 +44,11 @@ for($k=0;$k<$quanti;$k++)
 }
 echo("</table>");
 curl_close($ch);
+	
+echo "<form id='forma' method='post' onsubmit='return funzione();'><br/>";
+echo "Numero elementi (1-50):<input type='text' value='$quanti' name='quanti'id='quanti'/>";
+echo "<input type='submit' value='Ricerca'/>";	
+echo "</form>"
 ?>
 
 </body>
