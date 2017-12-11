8858438c3b6c2c773dc9b7edb61fbe620d201afa
salalorenzo <32730528+salalorenzo@users.noreply.github.com>
2017-12-11 18:03:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ echo "<th>Nome</th>";
 echo "<th>Latitudine</th>";
 echo "<th>Longitudine</th>";
 echo "</tr>";
-for($k=0;$k<40;$k++)
+for($k=0;$k<count(venues);$k++)
 {
 		echo("<tr>");
 		echo("<td>").$risposta->response->venues[$k]->name."</td>";
